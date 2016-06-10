1fd07d676034142885a0e684681e8c00726784f5
Heather Booker <Heather Booker>
2016-06-09 19:36:28 -0700

    fix weird quotes

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
       }
     }, {
       test: /\.scss$/,
-      loaders: [“style”, “css”, “sass”]
+      loaders: ["style", "css", "sass"]
     }, {
       test: /\.svg$/,
       loader: ‘file’
